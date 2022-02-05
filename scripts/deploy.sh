9abaa5fd6427a2ddb583d7bcfe7b8037d5922d21
jinia91 <creee77@gmail.com>
2022-02-06 07:18:06 +0900

    쉘 스크립트 수정중

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -18,7 +18,7 @@ if [ -z "$CURRENT_PID" ]; then
         echo ">현재 구동중인 애플리케이션이 없으므로 종료하지 않습니다"
 else
         echo "fuser -k 8080/tcp"
-#        kill -15 $CURRENT_PID
+#       kill -15 $CURRENT_PID
         fuser -k 8080/tcp
         sleep 5
 fi
