757bb211643c09ca5abd773461cc9968ff7d8381
jinia91 <creee77@gmail.com>
2022-02-06 04:53:37 +0900

    배포 쉘스크립트 수정

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -35,5 +35,5 @@ chmod +x $JAR_NAME
 echo "> $JAR_NAME 실행"
 
 nohup java -jar \
-  -Dspring.config.location=classpath:/application.yml \
+  -Dspring.config.location=/home/ec2-user/app/application.yml \
   $JAR_NAME > $REPOSITORY/nohup.out 2>&1 &
