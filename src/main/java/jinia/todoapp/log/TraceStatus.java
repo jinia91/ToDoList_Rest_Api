6c5fcb504bd102aba35f644dfa926750bff65c61
jinia91 <creee77@gmail.com>
2022-02-05 21:33:44 +0900

    로그 추적기 모듈 추가

---
+++ src/main/java/jinia/todoapp/log/TraceStatus.java
@@ -0,0 +1,12 @@
+package jinia.todoapp.log;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@AllArgsConstructor
+@Getter
+public class TraceStatus {
+    private TraceId traceId;
+    private Long startTimesMs;
+    private String message;
+}
