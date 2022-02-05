19884f6291044df4dc164cd14a4b5b847dc03297
jinia91 <creee77@gmail.com>
2022-02-06 04:35:54 +0900

    cd테스트

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -10,7 +10,7 @@ cp $REPOSITORY/zip/*.jar $REPOSITORY/
 
 echo "> 현재 구동중인 애플리케이션 pid 확인"
 
-CURRENT_PID=$(pgrep -fl springboot-blog | grep jar | awk '{print $1}')
+CURRENT_PID=$(pgrep -fl todo | grep jar | awk '{print $1}')
 
 echo "현재 구동중인 애플리케이션 pid: $CURRENT_PID"
 
@@ -34,4 +34,6 @@ chmod +x $JAR_NAME
 
 echo "> $JAR_NAME 실행"
 
-nohup java -jar $REPOSITORY/nohup.out 2>&1 &
+nohup java -jar \
+  -Dspring.config.location=classpath:/application.yml \
+  $JAR_NAME > $REPOSITORY/nohup.out 2>&1 &
