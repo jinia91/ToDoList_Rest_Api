f067753634585c4e43088b0888a390e9c827e21f
jinia91 <creee77@gmail.com>
2022-02-06 14:16:45 +0900

    운영 환경에서 테스트 코드 작동 테스트

--- src/test/java/jinia/todoapp/infra/TestDbCleaner.java
+++ src/test/java/jinia/todoapp/infra/TestDbCleaner.java
@@ -1,61 +1,61 @@
-//package jinia.todoapp.infra;
-//
-//import org.hibernate.Session;
-//import org.springframework.beans.factory.InitializingBean;
-//import org.springframework.context.annotation.Profile;
-//import org.springframework.stereotype.Component;
-//
-//import javax.persistence.EntityManager;
-//import javax.persistence.PersistenceContext;
-//import java.sql.Connection;
-//import java.sql.ResultSet;
-//import java.sql.SQLException;
-//import java.sql.Statement;
-//import java.util.ArrayList;
-//import java.util.List;
-//
-//@Component
-//@Profile("test")
-//public class TestDbCleaner implements InitializingBean {
-//
-//    @PersistenceContext
-//    private EntityManager entityManager;
-//
-//    private List<String> tableNames;
-//
-//    @Override
-//    public void afterPropertiesSet() {
-//        entityManager.unwrap(Session.class)
-//                .doWork(this::extractTableNames);
-//    }
-//    private void extractTableNames(Connection conn) throws SQLException {
-//        List<String> tableNames = new ArrayList<>();
-//
-//        ResultSet tables = conn
-//                .getMetaData()
-//                .getTables(conn.getCatalog(), null, "%", new String[]{"TABLE"});
-//
-//        while(tables.next()) {
-//            tableNames.add(tables.getString("table_name"));
-//        }
-//
-//        this.tableNames = tableNames;
-//    }
-//
-//    public void execute() {
-//        entityManager.unwrap(Session.class)
-//                .doWork(this::cleanUpDatabase);
-//    }
-//
-//    private void cleanUpDatabase(Connection conn) throws SQLException {
-//        Statement statement = conn.createStatement();
-//        statement.executeUpdate("SET REFERENTIAL_INTEGRITY FALSE");
-//
-//        for (String tableName : tableNames) {
-//            statement.executeUpdate("TRUNCATE TABLE " + tableName);
-//            statement.executeUpdate(String.format("ALTER TABLE %s ALTER COLUMN %s_ID RESTART WITH 1",tableName,tableName));
-//        }
-//
-//        statement.executeUpdate("SET REFERENTIAL_INTEGRITY TRUE");
-//    }
-//}
+package jinia.todoapp.infra;
+
+import org.hibernate.Session;
+import org.springframework.beans.factory.InitializingBean;
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Component;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
+
+@Component
+@Profile("test")
+public class TestDbCleaner implements InitializingBean {
+
+    @PersistenceContext
+    private EntityManager entityManager;
+
+    private List<String> tableNames;
+
+    @Override
+    public void afterPropertiesSet() {
+        entityManager.unwrap(Session.class)
+                .doWork(this::extractTableNames);
+    }
+    private void extractTableNames(Connection conn) throws SQLException {
+        List<String> tableNames = new ArrayList<>();
+
+        ResultSet tables = conn
+                .getMetaData()
+                .getTables(conn.getCatalog(), null, "%", new String[]{"TABLE"});
+
+        while(tables.next()) {
+            tableNames.add(tables.getString("table_name"));
+        }
+
+        this.tableNames = tableNames;
+    }
+
+    public void execute() {
+        entityManager.unwrap(Session.class)
+                .doWork(this::cleanUpDatabase);
+    }
+
+    private void cleanUpDatabase(Connection conn) throws SQLException {
+        Statement statement = conn.createStatement();
+        statement.executeUpdate("SET REFERENTIAL_INTEGRITY FALSE");
+
+        for (String tableName : tableNames) {
+            statement.executeUpdate("TRUNCATE TABLE " + tableName);
+            statement.executeUpdate(String.format("ALTER TABLE %s ALTER COLUMN %s_ID RESTART WITH 1",tableName,tableName));
+        }
+
+        statement.executeUpdate("SET REFERENTIAL_INTEGRITY TRUE");
+    }
+}
